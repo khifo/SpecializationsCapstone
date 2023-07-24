103449ae0fce493f51e71dd3d7a4bd692a7257ea
khifo <khifo09@yahoo.com>
2023-07-24 16:51:54 -0600

    concepts and registration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import Home from './components/home'
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/home';
+import Schedule from './components/schedule';
+import Concepts from './components/concepts';
+import Registration from './components/registration';
 
 function App() {
   return (
-    <div className='App'>
-        <h1>Yo</h1>
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/schedule" component={Schedule} />
+        <Route path="concepts" component={Concepts} />
+        <Route path="/registration" component={Registration} /> 
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
+
--- src/components/registration.js
+++ src/components/registration.js
@@ -2,35 +2,36 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../components/'
-const Registration = () => {
-  return (
-    <div>
-      <Header />
-      <h1>Registration</h1>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="phone">Phone Number:</label>
-        <input type="text" id="phone" name="phone" required />
-        <label htmlFor="team">Team:</label>
-        <select id="team" name="team">
-          <option value="team-a">Team A</option>
-          <option value="team-b">Team B</option>
-          <option value="team-c">Team C</option>
-        </select>
-        <label htmlFor="position">Position:</label>
-        <input type="text" id="position" name="position" required />
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message"></textarea>
-        <input type="submit" value="Register" />
-      </form>
-      <button id="payButton">Pay Now</button>
-      <Footer />
-    </div>
-  );
-};
+import '../components/registration.js'
+
+function Registration() {
+    return (
+        <div>
+            <Header />
+            <h1>Registration</h1>
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+                <label htmlFor="phone">Phone Number:</label>
+                <input type="text" id="phone" name="phone" required />
+                <label htmlFor="team">Team:</label>
+                <select id="team" name="team">
+                    <option value="team-a">Team A</option>
+                    <option value="team-b">Team B</option>
+                    <option value="team-c">Team C</option>
+                </select>
+                <label htmlFor="position">Position:</label>
+                <input type="text" id="position" name="position" required />
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message"></textarea>
+                <input type="submit" value="Register" />
+            </form>
+            <button id="payButton">Pay Now</button>
+            <Footer />
+        </div>
+    );
+}
 
 export default Registration;
