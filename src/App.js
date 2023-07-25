2b3f58b033b40060d687b305f7e85a6a9a5d0995
khifo <khifo09@yahoo.com>
2023-07-25 14:35:05 -0600

    route and switch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import Schedule from './components/schedule';
 import Concepts from './components/concepts';
 import Registration from './components/registration';
 
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
+      <Routes>
+        <Route path="/" component={Home} />
         <Route path="/schedule" component={Schedule} />
         <Route path="concepts" component={Concepts} />
         <Route path="/registration" component={Registration} /> 
-      </Switch>
+        </Routes>
     </Router>
   );
 };
